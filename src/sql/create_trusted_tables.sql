cab710039dcbe86571a2531df08a917dc2020faf
SergioJCAlves <sergiojrdacostaalves@gmail.com>
2025-01-21 23:27:35 -0300

    Update create_trusted_tables.sql

--- src/sql/create_trusted_tables.sql
+++ src/sql/create_trusted_tables.sql
@@ -1,1 +1,21 @@
+CREATE OR REPLACE TABLE `ps-eng-dados-ds3x.sergio_maxpayne.icc_trusted` AS
+SELECT
+    ano_mes,
+    indice,
+    variacao_mes,
+    variacao_ano,
+    load_timestamp
+FROM `ps-eng-dados-ds3x.sergio_maxpayne.icc_raw`
+WHERE indice IS NOT NULL       -- Exclui índices nulos
+GROUP BY ano_mes, indice, variacao_mes, variacao_ano, load_timestamp;
 
+CREATE OR REPLACE TABLE `ps-eng-dados-ds3x.sergio_maxpayne.icf_trusted` AS
+SELECT
+    ano_mes,
+    indice,
+    variacao_mes,
+    variacao_ano,
+    load_timestamp
+FROM `ps-eng-dados-ds3x.sergio_maxpayne.icf_raw`
+WHERE indice IS NOT NULL       -- Exclui índices nulos
+GROUP BY ano_mes, indice, variacao_mes, variacao_ano, load_timestamp;
